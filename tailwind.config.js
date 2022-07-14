2a37f477a5bdb1e1217153cc99bce119c4d3bb4d
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-14 12:28:57 +0200

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}",],
     theme: {
+        fontFamily:
+            {
+                sans: ["Quicksand", "sans-serif"],
+                serif: ["Cormorant Garamond", "serif"]
+            },
         extend: {},
     },
     plugins: [],
